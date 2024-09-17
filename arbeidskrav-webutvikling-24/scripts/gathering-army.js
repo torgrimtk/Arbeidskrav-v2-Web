e39bd72e1c5e500fc461ea764f29590635e1e760
Lasse <lasse.bjornerud91@gmail.com>
2024-09-17 20:42:56 +0100

    lagt til litt mer i army og html

--- arbeidskrav-webutvikling-24/scripts/gathering-army.js
+++ arbeidskrav-webutvikling-24/scripts/gathering-army.js
@@ -1,10 +1,26 @@
 const warriors = [
     {
-        name:"Snake",
+        id: 0,
+        name: "Snake",
         image: "warrior-1.jpg"
     },
     {
-        name:"Giant",
+        id: 1,
+        name: "Giant",
         image:"warrior-2.jpg"
     }
 ];
+const animals = [
+    {
+        id: 0,
+        name: "Horse",
+        image: "horse.png"
+    }
+];
+const machine = [
+    {
+        id: 0,
+        name: "catapult",
+        image: "catapult.png"
+    }
+]
