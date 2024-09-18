c857ee9cdc3c83e3903058eedd47440c2ce9224d
Torgrim K <torgrimknaedal@gmail.com>
2024-09-18 11:22:47 +0200

    La inn en del fixes på JS filen "gathering-army.js ":D"

--- arbeidskrav-webutvikling-24/scripts/gathering-army.js
+++ arbeidskrav-webutvikling-24/scripts/gathering-army.js
@@ -1,4 +1,4 @@
-const warriors = [
+const warriorsData = [
     {
         id: 0,
         name: "Snake",
@@ -10,17 +10,39 @@ const warriors = [
         image:"warrior-2.jpg"
     }
 ];
-const animals = [
+const animalsData = [
     {
         id: 0,
         name: "Horse",
         image: "horse.png"
     }
 ];
-const machine = [
+const machineData = [
     {
         id: 0,
         name: "catapult",
         image: "catapult.png"
     }
 ]
+
+const shopSection = document.getElementById("army-shop");
+
+const createArmy = (title, items, className) => {
+    return `
+        <article class="${className}">
+            <h3>${title}</h3>
+            ${items.map(item => `
+              <div>
+                <img class="${className}-img" src="images/${items.image}" alt="${item.name}">
+                <button>Buy ${item.name}</button>
+              </div>  
+            `).join('')}
+        </article>
+    `;
+}; 
+
+shopSection.innerHTML = `
+    ${createCategoryHTML('Warriors', warriors, 'warriors')}
+    ${createCategoryHTML('Animals', animals, 'animals')}
+    ${createCategoryHTML('War Machines', machines, 'war-machines')}
+`;
