6f95315de8a17510d5d4ca7877a11853f0ed7879
Lasse <lasse.bjornerud91@gmail.com>
2024-09-26 20:15:09 +0200

    delt opp kode i module og ryddet litt i kdoe

--- arbeidskrav-webutvikling-24/scripts/gathering-army.js
+++ arbeidskrav-webutvikling-24/scripts/gathering-army.js
@@ -1,28 +1,4 @@
-const warriorsData = [
-    { id: 0, name: "Berserker", image: "warrior-2.jpg", price: 1000 },
-    { id: 1, name: "Shieldmaiden", image: "warrior-1.jpg", price: 800 },
-    { id: 2, name: "Colossus", image: "warrior-3.jpg", price: 800 },
-    { id: 3, name: "Warrior", image: "warrior-4.jpg", price: 700 },
-    { id: 4, name: "Rogue", image: "warrior-5.jpg", price: 1500 },
-    { id: 5, name: "Druid", image: "warrior-6.jpg", price: 400 },
-    { id: 6, name: "Warlock", image: "warrior-1.jpg", price: 300 },
-    { id: 7, name: "Healer", image: "warrior-2.jpg", price: 350 }
-];
-
-const animalsData = [
-    { id: 0, name: "Horse", image: "horse.png", price:350},
-    { id: 1, name: "Elephant", image: "elephant.png", price:400},
-    { id: 2, name: "Horse", image: "horse.png", price:350},
-    { id: 3, name: "Elephant", image: "elephant.png", price:400}
-];
-
-const machinesData = [
-    { id: 0, name: "Catapult", image: "catapult.png", price: 1200, woodCost: 400, ironCost: 100 },
-    { id: 1, name: "Cannon", image: "cannon.png", price: 1200, woodCost: 100, ironCost: 300 },
-    { id: 2, name: "Catapult", image: "catapult.png", price: 700, woodCost: 400, ironCost: 100 },
-    { id: 3, name: "Cannon", image: "cannon.png", price: 800, woodCost: 100, ironCost: 300 }
-];
-
+import ArmyModule from "./utilities/units.js";
 
 const basketItems = document.getElementById("basket-items");
 const shopSection = document.getElementById("main-container");
@@ -31,10 +7,10 @@ let currentGold = parseInt(localStorage.getItem("gold")) || 0;
 let currentIron = parseInt(localStorage.getItem("iron")) || 0;
 let currentWood = parseInt(localStorage.getItem("wood")) || 0;
 
-//document.getElementById("gold-display").textContent = `Current gold: ${currentGold}g`;
 document.getElementById("currentGold").textContent = currentGold;
 document.getElementById("currentIron").textContent = currentIron;
 document.getElementById("currentWood").textContent = currentWood;
+
 const purchasedItems = {};
 
 const createArmy = (title, items, className) => {
@@ -66,6 +42,14 @@ const createArmy = (title, items, className) => {
         </article>
     `;
 };
+
+(() =>{
+    const allArmy = ArmyModule.getAll();
+
+    document.getElementById("warrior-section").innerHTML = createArmy('Warriors', allArmy.warriors, 'warriors');
+    document.getElementById("animal-section").innerHTML = createArmy('Animals', allArmy.animals, 'animals');
+    document.getElementById("war-machine-section").innerHTML = createArmy('War Machines', allArmy.machines, 'war-machines');
+})();
 
 shopSection.addEventListener("click", (e) => {
     if (e.target.classList.contains("buy-btn")) {
---
+++ arbeidskrav-webutvikling-24/scripts/your-army.js
@@ -0,0 +1,3 @@
+let currentGold = parseInt(localStorage.getItem("gold")) || 0;
+let currentIron = parseInt(localStorage.getItem("iron")) || 0;
+let currentWood = parseInt(localStorage.getItem("wood")) || 0;
